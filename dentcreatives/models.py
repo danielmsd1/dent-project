211aa59c5496c48a4c995f7dbb9f5a912ab6ca75
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-07-20 09:30:03 +0300

    feat:add homedetail model

--- dentcreatives/models.py
+++ dentcreatives/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class HomeDetail(models.Model):
+	title = models.CharField(max_length=200)
+	cover_image = models.ImageField(upload_to='images/')
+	about_text = models.TextField()
