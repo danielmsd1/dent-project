0c1e78ae13e9e98eb9eb34798e5e9f905b91bfb0
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-07-20 19:33:37 +0300

    refactor: allow null and blank for models

--- dentcreatives/models.py
+++ dentcreatives/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class HomeDetail(models.Model):
 	title = models.CharField(max_length=200)
-	cover_image = models.ImageField(upload_to='images/')
+	cover_image = models.ImageField(upload_to='images/',blank=True,null=True)
 	about_text = models.TextField()
 
 	def __str__(self):
 		return self.title
 
 class Social(models.Model):
-	facebook = models.URLField()
-	instagram = models.URLField()
-	twitter = models.URLField()
+	facebook = models.URLField(blank=True, null=True)
+	instagram = models.URLField(blank=True, null=True)
+	twitter = models.URLField(blank=True, null=True)
