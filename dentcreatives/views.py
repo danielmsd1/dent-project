7f33c3f82ce06c8a79ba2e87505dee5cb9e9b34d
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-07-20 22:42:38 +0300

    feat: add models for services

--- dentcreatives/views.py
+++ dentcreatives/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request,'dentcreatives/home.html')
+	return render(request,'dentcreatives/learning.html')
 
 
 def portfolio(request):
--- services/models.py
+++ services/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+	GRAPHICDESIGN = 'GD'
+	MOTIONGRAPHICS = 'MG'
+	SERVICES_OPTION = [
+		(GRAPHICDESIGN,'Graphic Design'),
+		(MOTIONGRAPHICS,'Motion Graphics')
+	]
+	name_of_service = models.CharField(
+		choices=SERVICES_OPTION,
+	)
+	service_text = models.TextField()
+	service_cover_image = models.ImageField(upload_to='images/', blank=True, null=True)
