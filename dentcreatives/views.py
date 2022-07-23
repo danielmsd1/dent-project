4ae76db6974457a83341d5b6d50e9e07ff823348
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-07-23 10:59:31 +0300

    fix: add static headers

--- dentcreatives/views.py
+++ dentcreatives/views.py
@@ -3,8 +3,12 @@ from .models import HomeDetail,Social
 # Create your views here.
 from projects.models import Project
 from services.models import Service
+from careers.models import Email
 
 def home(request):
+	if request.method == 'POST':
+		print(request.POST)
+
 	homedetail = HomeDetail.objects
 	social = Social.objects
 	project = Project.objects
@@ -14,4 +18,5 @@ def home(request):
 		'social':social,
 		'project':project,
 		'service':Service.objects,
+		'email':Email.objects,
 		})
