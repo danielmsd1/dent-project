a8f338d24c53ccb838f7b6d5bbe7a8ac17b36876
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-08-03 19:43:20 +0300

    refactor: remove unnecessary information

--- dent/settings.py
+++ dent/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dentdb',
         'USER': 'postgres',
-        'PASSWORD': 'Qwetu8934',
+        'PASSWORD': '**********',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
