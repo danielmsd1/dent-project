cc8780623e1ec4e78c8e85df440e809ec13124c0
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-08-09 13:02:31 +0300

    fix: add images

--- dent/settings.py
+++ dent/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dentdb',
         'USER': 'postgres',
-        'PASSWORD': '**********',
+        'PASSWORD': 'Qwetu8934',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
